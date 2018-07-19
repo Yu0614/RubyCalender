398d3388dfee5f5abcc49ad985be5acd087da380
Yu0614 <azu369yu@gmail.com>
2018-07-19 23:46:21 +0900

    add holidays function

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
-
+require_relative 'holiday_jp'
+require_relative 'holiday_japan'
+require 'holiday_jp'
+require 'holiday_japan'
 # Initialize the Rails application.
 Rails.application.initialize!
